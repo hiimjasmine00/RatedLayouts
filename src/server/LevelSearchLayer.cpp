28fd207066eddc64035c3132c101fd01a21c56a0
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2025-11-17 00:28:26 +0800

    added featured levels ig

--- src/server/LevelSearchLayer.cpp
+++ src/server/LevelSearchLayer.cpp
@@ -28,6 +28,14 @@ class $modify(RLLevelSearchLayer, LevelSearchLayer)
                 this,
                 menu_selector(RLLevelSearchLayer::onSuggestedLevelsButton));
             quickSearchMenu->addChild(tabButton);
+
+            auto ratedButtonSprite = SearchButton::create("GJ_longBtn04_001.png", "Rated Layouts", 0.5f, "GJ_sFollowedIcon_001.png");
+            auto ratedTabButton = CCMenuItemSpriteExtra::create(
+                ratedButtonSprite,
+                this,
+                menu_selector(RLLevelSearchLayer::onRatedLevelsButton));
+            quickSearchMenu->addChild(ratedTabButton);
+
             quickSearchMenu->updateLayout();
         }
         return true;
@@ -50,12 +58,10 @@ class $modify(RLLevelSearchLayer, LevelSearchLayer)
                         auto json = jsonResult.unwrap();
                         std::string levelIDs;
                         bool first = true;
-
-                        // Parse level IDs from response
                         if (json.contains("levelIds")) {
                             auto levelsArr = json["levelIds"];
                             
-                            // Iterate over all level IDs in the array
+                            // iterate
                             for (auto levelIDValue : levelsArr) {
                                 auto levelID = levelIDValue.as<int>();
                                 if (levelID) {
@@ -67,15 +73,60 @@ class $modify(RLLevelSearchLayer, LevelSearchLayer)
                         }
 
                         if (!levelIDs.empty()) {
-                            // Create GJSearchObject with level IDs
                             auto searchObject = GJSearchObject::create(SearchType::Type19, levelIDs);
-
-                            // Create LevelBrowserLayer
                             auto browserLayer = LevelBrowserLayer::create(searchObject);
                             auto scene = CCScene::create();
                             scene->addChild(browserLayer);
+                            auto transitionFade = CCTransitionFade::create(0.5f, scene);
+                            CCDirector::sharedDirector()->pushScene(transitionFade);
+                        } else {
+                            log::warn("No levels found in response");
+                        }
+                    } else {
+                        log::error("Failed to parse response JSON");
+                    }
+                } else {
+                    log::error("Failed to fetch levels from server");
+                } });
+    }
 
-                            // Push scene with transition
+    void onRatedLevelsButton(CCObject *sender)
+    {
+        m_fields->m_webTask.cancel();
+
+        web::WebRequest()
+            .param("type", 0)
+            .param("amount", 1000)
+            .get("https://gdrate.arcticwoof.xyz/getLevels")
+            .listen([this](web::WebResponse *res)
+                    {
+                if (res && res->ok()) {
+                    auto jsonResult = res->json();
+                    
+                    if (jsonResult) {
+                        auto json = jsonResult.unwrap();
+                        std::string levelIDs;
+                        bool first = true;
+
+                        if (json.contains("levelIds")) {
+                            auto levelsArr = json["levelIds"];
+                            
+                            // iterate
+                            for (auto levelIDValue : levelsArr) {
+                                auto levelID = levelIDValue.as<int>();
+                                if (levelID) {
+                                    if (!first) levelIDs += ",";
+                                    levelIDs += numToString(levelID.unwrap());
+                                    first = false;
+                                }
+                            }
+                        }
+
+                        if (!levelIDs.empty()) {
+                            auto searchObject = GJSearchObject::create(SearchType::Type19, levelIDs);
+                            auto browserLayer = LevelBrowserLayer::create(searchObject);
+                            auto scene = CCScene::create();
+                            scene->addChild(browserLayer);
                             auto transitionFade = CCTransitionFade::create(0.5f, scene);
                             CCDirector::sharedDirector()->pushScene(transitionFade);
                         } else {
